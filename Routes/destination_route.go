3d5e6da764b64dc047e3359ee68fe1cc499ae86a
sivm99 <git.v7gr4@simplelogin.fr>
2024-10-15 18:13:12 +0530

    New Routes Since thats what i'm used to

--- Routes/destination_route.go
+++ Routes/destination_route.go
@@ -8,10 +8,10 @@ import (
 )
 
 func DestinationRouter(mux *http.ServeMux) {
-	mux.HandleFunc("GET /api/v2/destinations", middlewares.Protect(destinations.ListDestinations))
-	mux.HandleFunc("GET /api/v2/destinations/{id}", middlewares.Protect(destinations.GetDestination))
-	mux.HandleFunc("GET /api/v2/destinations/{id}/verify", middlewares.Protect(destinations.VerifyDestination))
-	mux.HandleFunc("POST /api/v2/destinations", middlewares.Protect(destinations.CreateDestination))
+	mux.HandleFunc("GET /api/v2/mail/destinations", middlewares.Protect(destinations.ListDestinations))
+	mux.HandleFunc("GET /api/v2/mail/destinations/{id}", middlewares.Protect(destinations.GetDestination))
+	mux.HandleFunc("GET /api/v2/mail/destinations/{id}/verify", middlewares.Protect(destinations.VerifyDestination))
+	mux.HandleFunc("POST /api/v2/mail/destinations", middlewares.Protect(destinations.CreateDestination))
 	// mux.HandleFunc("PATCH /api/v2/destinations/{id}", middlewares.Protect(destinations.UpdateDestination))
-	mux.HandleFunc("DELETE /api/v2/destinations/{id}", middlewares.Protect(destinations.DeleteDestination))
+	mux.HandleFunc("DELETE /api/v2/mail/destinations/{id}", middlewares.Protect(destinations.DeleteDestination))
 }
--- Routes/rule_route.go
+++ Routes/rule_route.go
@@ -8,10 +8,10 @@ import (
 )
 
 func RuleRouter(mux *http.ServeMux) {
-	mux.HandleFunc("GET /api/v2/rules", middlewares.Protect(rules.ListRules))
-	mux.HandleFunc("GET /api/v2/rules/{id}", middlewares.Protect(rules.GetRule))
-	mux.HandleFunc("POST /api/v2/rules", middlewares.Protect(rules.CreateRule))
-	mux.HandleFunc("PATCH /api/v2/rules/{id}", middlewares.Protect(rules.UpdateRule))
-	mux.HandleFunc("PATCH /api/v2/rules/{id}/toggle", middlewares.Protect(rules.ToggleRule))
-	mux.HandleFunc("DELETE /api/v2/rules/{id}", middlewares.Protect(rules.DeleteRule))
+	mux.HandleFunc("GET /api/v2/mail/rules", middlewares.Protect(rules.ListRules))
+	mux.HandleFunc("GET /api/v2/mail/rules/{id}", middlewares.Protect(rules.GetRule))
+	mux.HandleFunc("POST /api/v2/mail/rules", middlewares.Protect(rules.CreateRule))
+	mux.HandleFunc("PATCH /api/v2/mail/rules/{id}", middlewares.Protect(rules.UpdateRule))
+	mux.HandleFunc("PATCH /api/v2/mail/rules/{id}/toggle", middlewares.Protect(rules.ToggleRule))
+	mux.HandleFunc("DELETE /api/v2/mail/rules/{id}", middlewares.Protect(rules.DeleteRule))
 }
