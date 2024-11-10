1460460dbfa94ab62dde683104d01d35b1cbab67
sivm99 <git.v7gr4@simplelogin.fr>
2024-11-11 00:22:13 +0530

    More Console logs

--- Controllers/Destinations/delete_destination.go
+++ Controllers/Destinations/delete_destination.go
@@ -2,6 +2,7 @@ package destinations
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strconv"
 	"sync"
@@ -21,17 +22,20 @@ func DeleteDestination(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewDecoder(r.Body).Decode(&requestBody)
 	if err != nil {
+		fmt.Println("Request body decode error:", err)
 		utils.SendErrorResponse(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
 
 	user, ok := utils.GetUserFromContext(r.Context())
 	if !ok {
+		fmt.Println("User context error: user not found in context")
 		utils.SendErrorResponse(w, "User not found", http.StatusUnauthorized)
 		return
 	}
 
 	if !utils.CheckPassword(requestBody.Password, user.Password) {
+		fmt.Println("Password verification failed for user:", user.Username)
 		utils.SendErrorResponse(w, "Password is required to delete a destination address", http.StatusUnauthorized)
 		return
 	}
@@ -39,47 +43,49 @@ func DeleteDestination(w http.ResponseWriter, r *http.Request) {
 	destinationIDStr := r.PathValue("id")
 	destinationIDInt, err := strconv.Atoi(destinationIDStr)
 	if err != nil {
+		fmt.Println("Destination ID conversion error:", err)
 		utils.SendErrorResponse(w, "Invalid destination ID", http.StatusBadRequest)
 		return
 	}
 	destinationID := uint32(destinationIDInt)
 	destination, err := repository.FindDestinationByID(destinationID)
 	if err != nil {
+		fmt.Println("Find destination error:", err)
 		utils.SendErrorResponse(w, "Destination not found", http.StatusNotFound)
 		return
 	}
 	if destination.Username != user.Username {
+		fmt.Println("Unauthorized deletion attempt by", user.Username, "for destination owned by", destination.Username)
 		utils.SendErrorResponse(w, "You are not allowed to delete this destination", http.StatusForbidden)
 		return
 	}
 
 	_, err = requests.DestinationRequest(`DELETE`, destination.DestinationEmail, destination.Username, destination.CloudflareDestinationID)
 	if err != nil {
+		fmt.Println("Destination DELETE request error:", err)
 		utils.SendErrorResponse(w, "Something went wrong", http.StatusInternalServerError)
 		return
 	}
 
 	err = repository.DeleteDestinationByID(destinationID, user.ID)
 	if err != nil {
+		fmt.Println("Delete destination from DB error:", err)
 		utils.SendErrorResponse(w, "Something went wrong", http.StatusInternalServerError)
 		return
 	}
 
-	// we will toggle all the rules to inactive and also toggle them in the rules table
 	domain := destination.Domain
-
 	rules, err := repository.FindActiveRulesByDestinationEmail(destination.DestinationEmail)
 	if err != nil {
 		if err == pgx.ErrNoRows {
-			// No rules found, we can proceed
 			utils.CreateSendResponse(w, nil, "Destination Deleted Successfully", http.StatusNoContent, "destination", user.Username)
 			return
 		}
+		fmt.Println("Find active rules error:", err)
 		utils.SendErrorResponse(w, "Something went wrong", http.StatusInternalServerError)
 		return
 	}
 
-	// Create a semaphore with buffer of 10 to limit concurrent operations
 	sem := make(chan struct{}, 10)
 	errChan := make(chan error, len(rules))
 	var wg sync.WaitGroup
@@ -88,34 +94,31 @@ func DeleteDestination(w http.ResponseWriter, r *http.Request) {
 		wg.Add(1)
 		go func(rule models.Rule) {
 			defer wg.Done()
-
-			// Acquire semaphore
 			sem <- struct{}{}
 			defer func() { <-sem }()
 
-			// Make the API request
 			err := requests.CreateRuleRequest(`PATCH`, rule.AliasEmail, rule.DestinationEmail, rule.Username, domain)
 			if err != nil {
+				fmt.Println("Rule PATCH request error for rule ID", rule.ID, ":", err)
 				errChan <- err
 				return
 			}
 
-			// Toggle the rule in database
 			err = repository.ToggleRuleByID(rule.ID)
 			if err != nil {
+				fmt.Println("Toggle rule error for rule ID", rule.ID, ":", err)
 				errChan <- err
 				return
 			}
 		}(rule)
 	}
 
-	// Wait for all goroutines to complete
 	wg.Wait()
 	close(errChan)
 
-	// Check for any errors
 	for err := range errChan {
 		if err != nil {
+			fmt.Println("Error in goroutine:", err)
 			utils.SendErrorResponse(w, "Something went wrong", http.StatusInternalServerError)
 			return
 		}
